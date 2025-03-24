4e02e7a7d3ebd4362e30931051418106f069ab09
tverskova <uliatverskova51480@gmail.com>
2025-03-24 18:58:24 +0300

    3

---
+++ tverskova/Migrations/20250324153429_CreateDatabase.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tverskova.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AcademicDegrees",
+                columns: table => new
+                {
+                    AcademicDegreeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    c_academic_degree_name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false, comment: "Название ученой степени (Кандидат наук, Доктор наук)")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_cd_academic_degree_academic_degree_id", x => x.AcademicDegreeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Staffers",
+                columns: table => new
+                {
+                    StaffId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    c_staff_name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false, comment: "Название должности (Преподаватель, Доцент, Профессор)")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_cd_staff_staff_id", x => x.StaffId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeadTeacherId = table.Column<int>(type: "int", nullable: false),
+                    HeadTeacherTeacherId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    TeacherId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    c_teacher_firstname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    c_teacher_lastname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    c_teacher_middlename = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AcademicDegreeId = table.Column<int>(type: "int", nullable: false),
+                    StaffId = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_cd_teacher_teacher_id", x => x.TeacherId);
+                    table.ForeignKey(
+                        name: "fk_cd_academic_degree_teacher_id",
+                        column: x => x.AcademicDegreeId,
+                        principalTable: "AcademicDegrees",
+                        principalColumn: "AcademicDegreeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_cd_staff_teacher_id",
+                        column: x => x.StaffId,
+                        principalTable: "Staffers",
+                        principalColumn: "StaffId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_cd_teacher_department_id",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Disciplines",
+                columns: table => new
+                {
+                    DisciplineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    c_discipline_name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false, comment: "Название дисциплины"),
+                    TeacherId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_cd_discipline_discipline_id", x => x.DisciplineId);
+                    table.ForeignKey(
+                        name: "fk_cd_discipline_teacher_id",
+                        column: x => x.TeacherId,
+                        principalTable: "Teachers",
+                        principalColumn: "TeacherId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Workloads",
+                columns: table => new
+                {
+                    WorkloadId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DisciplineId = table.Column<int>(type: "int", nullable: false),
+                    c_workload_hours = table.Column<int>(type: "int", nullable: false, comment: "Количество часов нагрузки по дисциплине")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_cd_workload_workload_id", x => x.WorkloadId);
+                    table.ForeignKey(
+                        name: "fk_cd_workload_discipline_id",
+                        column: x => x.DisciplineId,
+                        principalTable: "Disciplines",
+                        principalColumn: "DisciplineId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_HeadTeacherTeacherId",
+                table: "Departments",
+                column: "HeadTeacherTeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Disciplines_TeacherId",
+                table: "Disciplines",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_AcademicDegreeId",
+                table: "Teachers",
+                column: "AcademicDegreeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_DepartmentId",
+                table: "Teachers",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_StaffId",
+                table: "Teachers",
+                column: "StaffId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workloads_DisciplineId",
+                table: "Workloads",
+                column: "DisciplineId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Teachers_HeadTeacherTeacherId",
+                table: "Departments",
+                column: "HeadTeacherTeacherId",
+                principalTable: "Teachers",
+                principalColumn: "TeacherId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Teachers_HeadTeacherTeacherId",
+                table: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Workloads");
+
+            migrationBuilder.DropTable(
+                name: "Disciplines");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+
+            migrationBuilder.DropTable(
+                name: "AcademicDegrees");
+
+            migrationBuilder.DropTable(
+                name: "Staffers");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
--- tverskova/Program.cs
+++ tverskova/Program.cs
@@ -7,44 +7,29 @@ using tverskova.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-try
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TeacherDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    builder.Logging.ClearProviders();
-    builder.Host.UseNLog();
-    // Add services to the container.
-
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    builder.Services.AddDbContext<TeacherDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default connection")));
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-catch(Exception ex)
-{
-    logger.Error(ex, "Stopped program, because of exception");
-}
-finally
-{
-    LogManager.Shutdown();
-}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
 
 
 
